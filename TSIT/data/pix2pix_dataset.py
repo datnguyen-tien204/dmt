4c5e7f8df42e83ecd80aa77a2afb9cd6a25c26cf
Dat Nguyen-Tien <118755598+datnguyen-tien204@users.noreply.github.com>
2026-03-20 10:29:10 +0700

    Fix instance tensor handling and add debug print

--- TSIT/data/pix2pix_dataset.py
+++ TSIT/data/pix2pix_dataset.py
@@ -13,27 +13,22 @@ class Pix2pixDataset(BaseDataset):
 
     def initialize(self, opt):
         self.opt = opt
-
         label_paths, image_paths, instance_paths = self.get_paths(opt)
-
         util.natural_sort(label_paths)
         util.natural_sort(image_paths)
         if not opt.no_instance:
             util.natural_sort(instance_paths)
-
         label_paths = label_paths[:opt.max_dataset_size]
         image_paths = image_paths[:opt.max_dataset_size]
         instance_paths = instance_paths[:opt.max_dataset_size]
-
         if not opt.no_pairing_check:
             for path1, path2 in zip(label_paths, image_paths):
                 assert self.paths_match(path1, path2), \
                     "The label-image pair (%s, %s) do not look like the right pair because the filenames are quite different. Are you sure about the pairing? Please see data/pix2pix_dataset.py to see what is going on, and use --no_pairing_check to bypass this." % (path1, path2)
-
         self.label_paths = label_paths
         self.image_paths = image_paths
         self.instance_paths = instance_paths
-
+        print("Length of instance_paths:", len(self.instance_paths))
         size = len(self.label_paths)
         self.dataset_size = size
 
@@ -50,54 +45,46 @@ class Pix2pixDataset(BaseDataset):
         return filename1_without_ext == filename2_without_ext
 
     def __getitem__(self, index):
-        # Label (Content) Image
         label_path = self.label_paths[index]
         label = Image.open(label_path)
         if self.opt.task != 'SIS':
             label = label.convert('RGB')
         params = get_params(self.opt, label.size)
-
         if self.opt.task != 'SIS':
             transform_label = get_transform(self.opt, params)
             label_tensor = transform_label(label)
         else:
             transform_label = get_transform(self.opt, params, method=Image.NEAREST, normalize=False)
             label_tensor = transform_label(label) * 255.0
-            label_tensor[label_tensor == 255] = self.opt.label_nc  # 'unknown' is opt.label_nc
-
-        # Real (Style) Image
+            label_tensor[label_tensor == 255] = self.opt.label_nc
         image_path = self.image_paths[index]
         assert self.paths_match(label_path, image_path), \
             "The label_path %s and image_path %s don't match." % \
             (label_path, image_path)
         image = Image.open(image_path)
         image = image.convert('RGB')
-
         transform_image = get_transform(self.opt, params)
         image_tensor = transform_image(image)
-
-        # if using instance maps
         if self.opt.no_instance:
             instance_tensor = 0
         else:
-            instance_path = self.instance_paths[index]
-            instance = Image.open(instance_path)
+            if len(self.instance_paths) > 0:
+                instance_path = self.instance_paths[index]
+                instance = self.read_image(instance_path)
+            else:
+                instance = None
             if instance.mode == 'L':
                 instance_tensor = transform_label(instance) * 255
                 instance_tensor = instance_tensor.long()
             else:
                 instance_tensor = transform_label(instance)
-
         input_dict = {'label': label_tensor,
                       'instance': instance_tensor,
                       'image': image_tensor,
                       'path': image_path,
                       'cpath': label_path
                       }
-
-        # Give subclasses a chance to modify the final output
         self.postprocess(input_dict)
-
         return input_dict
 
     def postprocess(self, input_dict):
